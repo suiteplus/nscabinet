ae59112d93e661a62fa1dbc9b447cafd2f2227ad
Willian Krueger <(personal)>
2015-09-29 13:39:50 -0300

    working draft core

--- nscabinet-restlet.js
+++ nscabinet-restlet.js
@@ -9,35 +9,90 @@ function post(datain) {
     if (!filepath) throw nlapiCreateError('No file name specified',true);
     if (!rootFolder) throw nlapiCreateError("No destination root path specified",true);
 
-    var fileFolder = rootFolder;
+    if (isNaN(Number(rootFolder))) {
+        var pathInfo = folderId(rootFolder,null,true);
+        rootFolder = pathInfo.folderid;
+    }
 
     //include windows' case (inverted slash)
-    var fname_split = filepath.replace(/[\\]/g,"/").split("/");
+    var info = folderId(filepath,rootFolder);
 
-    var len = fname_split.length -1;
+    nlapiLogExecution("DEBUG","folder",info.folderid);
+
+    if (info.filename) {
+        var file = nlapiCreateFile(info.filename,info.nsfileext,body);
+        file.setFolder(info.folderid);
+        var r = JSON.stringify(nlapiSubmitFile(file));
+        nlapiLogExecution("ERROR","up!",r);
+        return { message : "Uploaded to file id " + r , fileid : r };
+    }
+
+}
+
+var EXT_TYPES = {
+    dwg :	'AUTOCAD',
+    bmp :	'BMPIMAGE',
+    csv :	'CSV',
+    xls :	'EXCEL',
+    swf :	'FLASH',
+    gif :	'GIFIMAGE',
+    gz :	'GZIP',
+    htm :	'HTMLDOC',
+    ico :	'ICON',
+    js :	'JAVASCRIPT',
+    jpg :	'JPGIMAGE',
+    eml :	'MESSAGERFC',
+    mp3 :	'MP3',
+    mpg :	'MPEGMOVIE',
+    mpp :	'MSPROJECT',
+    pdf :	'PDF',
+    pjpeg :	'PJPGIMAGE',
+    txt :	'PLAINTEXT',
+    png :	'PNGIMAGE',
+    ps :	'POSTSCRIPT',
+    ppt :	'POWERPOINT',
+    mov :	'QUICKTIME',
+    rtf :	'RTF',
+    sms :	'SMS',
+    css :	'STYLESHEET',
+    tiff :	'TIFFIMAGE',
+    vsd :	'VISIO',
+    doc :	'WORD',
+    xml :	'XMLDOC',
+    zip :	'ZIP'
+}
+
+
+function folderId(path,root,isFolder) {
+
+    "use strict"
+
+    //windows fix
+    var fname = path.replace(/[\\]/g,"/");
+    //pass folder instead of file as arg
+    if (isFolder && fname.charAt(fname.length-1) != "/") {
+        fname += "/";
+    }
+    var fname_split = fname.split("/");
+    var folderId = root;
+
+    var len = fname_split.length - 1;
     for ( var it = 0 ; it < len ; it++ ) {
         var item = fname_split[it];
         if (!item.length) continue;
         var res_folder = nlapiSearchRecord( "folder", null ,
-            [ [ "name" , "is" , item ] , "and" , [ "parent" , "is" , fileFolder ] ]
+            [ [ "name" , "is" , item ] , "and" , [ "parent" , "anyof" , folderId || '@NONE@' ] ]
         );
         if (!res_folder) throw nlapiCreateError("Folder " + item + " not found!",true);
-        fileFolder = res_folder[0].getId();
+        folderId = res_folder[0].getId();
     }
 
-    var filename = fname_split[fname_split.length-1];
-    var fileext = filename.substr(filename.lastIndexOf(".")+1);
-    var nsfileext = "JAVASCRIPT";
-    if ( fileext == "html" ) nsfileext = "HTMLDOC";
+    var out = {};
+    out.folderid = folderId;
+    out.filename = fname_split[fname_split.length-1];
+    out.fileext = out.filename.substr(out.filename.lastIndexOf(".")+1);
+    out.nsfileext = EXT_TYPES[out.fileext] || "PLAINTEXT";
 
-    nlapiLogExecution("DEBUG","folder",fileFolder);
-
-    if (filename) {
-        var file = nlapiCreateFile(filename,nsfileext,body);
-        file.setFolder(fileFolder);
-        var r = JSON.stringify(nlapiSubmitFile(file));
-        nlapiLogExecution("ERROR","up!",r);
-        return r;
-    }
+    return out;
 
 }
--- nscabinet.js
+++ nscabinet.js
@@ -1,9 +1,10 @@
-'use strict'
+"use strict"
 
 var request = require('request'),
-    through = require('through2')
+    through = require('through2'),
+    jsonStream = require('JSONStream')
 
-var upsuite = ( params ) => {
+module.exports = ( params ) => {
 
     if (!params.email) throw 'No email defined'
     if (!params.password) throw 'No password defined'
@@ -16,26 +17,36 @@ var upsuite = ( params ) => {
 
     var nlauthRolePortion = ( params.role ) ? `,nlauth_role=${params.role}` : ''
 
-    var fileTransform = through.obj( (chunk,enc,callback) => {
-        var a = 0;
-        callback()
-    })
+    return through.obj( function(chunk,enc,callback) {
 
-    /*
-    request({
-        url : `https://rest.${params.realm}//app/site/hosting/restlet.nl` ,
-        qs : {
-            script : params.script ,
-            deploy : params.deploy
-        } ,
-        method : params.method ,
-        headers : {
-            authorization : `NLAuth nlauth_account=${params.account},nlauth_email=${params.email},nlauth_signature=${params.password}${nlauthRolePortion}`
-        } ,
-        json : {
-         //   filename
-        }
+        var that = this;
+        var path = chunk.path.substr(chunk.dirname.length)
+
+        request({
+            url : `https://rest.${params.realm}/app/site/hosting/restlet.nl` ,
+            qs : {
+                script : params.script ,
+                deploy : params.deployment
+            } ,
+            method : params.method ,
+            headers : {
+                authorization : `NLAuth nlauth_account=${params.account},nlauth_email=${params.email},nlauth_signature=${params.password}${nlauthRolePortion}`
+            } ,
+            json : {
+                filename : path ,
+                content : chunk.contents.toString('base64') ,
+                rootfolder : params.rootPath
+            }
+        }).on('response' , response => {
+
+            chunk.nscabinetResponse = response
+            that.push(chunk)
+            response.pipe(jsonStream.parse('error.code')).pipe(process.stdout)
+            response.pipe(jsonStream.parse('message')).pipe(process.stdout)
+            callback()
+
+        })
+
     })
-    */
 
 }
